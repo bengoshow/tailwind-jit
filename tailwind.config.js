60e71aba5a680f3542ce5045cc8752b67a86c69a
Ben Goshow <ben@fishhook.us>
2021-11-04 16:02:22 -0400

    adding JIT breaks build/css/main.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 // This file controls how Tailwind processes your CSS. For details, see
 // https://tailwindcss.com/docs/configuration
 
-module.exports = 
-{
+module.exports = {
+  mode: 'jit',
   //
   // WARNING: CodeKit overwrites all properties of the "purge" object (except those below) with values from the UI.
   // Visit [Project Settings > Tools > PurgeCSS] to specify content and options. The values below can be
@@ -13,21 +13,20 @@ module.exports =
   //   layers: ['base', 'components', 'utilities']
   // },
 
-
   //
   // All other TailwindCSS options are 100% under your control. Edit this config file as shown in the Tailwind Docs
   // to enable the settings or customizations you need.
-  // 
+  //
   theme: {
-    extend: {}
+    extend: {},
   },
 
   variants: {},
 
   //
   // If you want to run any Tailwind plugins (such as 'tailwindcss-typography'), simply install those into the Project via the
-  // Packages area in CodeKit, then pass their names (and, optionally, any configuration values) here. 
+  // Packages area in CodeKit, then pass their names (and, optionally, any configuration values) here.
   // Full file paths are not necessary; CodeKit will find them.
   //
-  plugins: []
-}
+  plugins: [],
+};
